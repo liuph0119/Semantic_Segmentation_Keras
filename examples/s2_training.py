63a77c1fa1e26a6b574d6e125b9134124168a7e4
liuph0119 <liuphhhh@foxmail.com>
2019-04-24 20:19:53 +0800

    Delete old invalid files
    Delete old invalid files

--- examples/s2_training.py
+++
@@ -1,8 +0,0 @@
-import os
-from core.utils.training_utils import parse_training_args, training_main
-
-
-if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    trainargs = parse_training_args()
-    training_main(trainargs)
--- examples/s3_predicting.py
+++
@@ -1,8 +0,0 @@
-import os
-from core.configures import PREDICTING_CONFIG
-from core.utils.predicting_utils import predict_main
-
-
-if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICE"] = "0"
-    predict_main(PREDICTING_CONFIG)
